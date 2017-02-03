5bd76f20030bacb91391c587f3d72161a7a35314
Luca Terzaroli <luca.terzaroli@opicino.it>
2017-02-03 19:17:21 +0100

    added some error message

--- app/controllers/fb_analyzer_controller.rb
+++ app/controllers/fb_analyzer_controller.rb
@@ -7,12 +7,16 @@ class FbAnalyzerController < ApplicationController
     if session[:oauth_token]
       pages = params['pages']
       limits = params['limits']
-        @post_in_pages = FbPost.posts_in_page(session[:oauth_token], pages, limits)
+      @post_in_pages = FbPost.posts_in_page(session[:oauth_token], pages, limits)
+
     end
     logger.info("POST: #{@post_in_pages}")
     respond_to do |format|
       format.html
       format.csv { send_data FbPost.to_csv(@post_in_pages) }
     end
+  rescue Koala::Facebook::ServerError, Koala::Facebook::ClientError => ex
+    flash[:error] = ex.message
+    redirect_to :root
   end
 end
