6698f1535cf6d26cc88db56cb8a75dd40787c47d
Luca Terzaroli <luca.terzaroli@opicino.it>
2017-02-04 16:40:34 +0100

    file fb_analyzer_index.js created

---
+++ app/assets/javascripts/fb_analyzer_index.js
@@ -0,0 +1,22 @@
+$(document).on('click', '.btn-add', function(e)
+{
+  e.preventDefault();
+
+  var currentEntry = $(this).parents('.param_row:last'),
+      newEntry = $(currentEntry.clone()).appendTo('.param_container:last');
+
+  newEntry.find('input').val('');
+  newEntry.find('.btn-add')
+      .removeClass('btn-default').addClass('btn-danger')
+      .removeClass('btn-add').addClass('btn-remove')
+      .html('<span class="glyphicon glyphicon-minus" aria-hidden="true"></span> Remove   ');
+  $('#fan_collector').validator('update');
+
+}).on('click', '.btn-remove', function(e)
+{
+  $(this).parents('.param_row:last').remove();
+
+  e.preventDefault();
+  $('#fan_collector').validator('update');
+  return false;
+});
