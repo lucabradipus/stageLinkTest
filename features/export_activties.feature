bd1a814df11c92ad9033c864499d5cede078a8c5
Luca Terzaroli <luca.terzaroli@opicino.it>
2017-02-23 18:36:52 +0100

    another cucumber test

--- features/export_activties.feature
+++ features/export_activties.feature
@@ -10,3 +10,14 @@ Feature: user can export to csv page activities
     Then I can log in
     And see the query form
 
+  Scenario Template: user can fill more than one line in the form
+    Given I am logged in
+    Then I can add more lines to the form
+    And I can fill page id with pageid <page> and limit <limit>
+    Then I  can export a csv file
+#    And the content of the csv wiil be |table|
+
+    Examples:
+      | page  | limit |
+      | first_page | 10    |
+
--- features/step_definitions/export_activities_steps.rb
+++ features/step_definitions/export_activities_steps.rb
@@ -16,3 +16,33 @@ Then(/^see the query form$/) do
   find('form', id: "fan_collector")
   find_link('sign_out')
 end
+
+
+
+Given(/^I am logged in$/) do
+  visit root_path
+  stub_request(:get, "https://graph.facebook.com/v2.8/me?access_token=fake_token&fields=id,name").
+    to_return(:status => 200, :body => "{ \"id\": \"111111111111111\", \"name\": \"Example User\"}", :headers => {})
+  find_link('sign_in').click
+end
+
+Then(/^I can add more lines to the form$/) do
+  find('button', class: 'btn-add').click
+end
+
+
+Then(/^I can fill page id with pageid (\w+) and limit (\d+)$/) do |arg1, arg2|
+  # sleep(100)
+  all('input[name="pages[][id]"]').each_with_index do |p, i|
+    p.set("#{arg1}#{i}")
+  end
+  all('input[name="pages[][limit]"]').each_with_index do |p, i|
+    p.set("#{arg2.to_i+i}")
+  end
+end
+
+Then(/^I  can export a csv file$/) do
+  find('button', text: 'Export to CSV').click
+end
+
+# expect(page.response_headers).to eq("application/csv")
